00ed9d989d1339b842a2919b98d5d0e756a5cc42
Brian Turner <brianturnercs@gmail.com>
2024-03-22 16:08:10 -0600

    changed JWT secret

--- auth_jwt.js
+++ auth_jwt.js
@@ -4,7 +4,8 @@ var ExtractJwt = require('passport-jwt').ExtractJwt;
 
 var opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
-opts.secretOrKey = process.env.SECRET_KEY;
+//opts.secretOrKey = process.env.SECRET_KEY;
+opts.secretOrKey = config.SECRET_KEY;
 
 passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
     var user = db.find(jwt_payload.id);
